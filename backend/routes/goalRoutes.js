52a2544b80414ef2d5933047652ca78ca254b5ad
BarbuAndreiIulian <andreibarbu681@gmail.com>
2022-07-01 19:27:51 +0300

    Initial commit

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getGoal,
+  postGoal,
+  updateGoal,
+  deleteGoal,
+} = require("./goalController");
+
+router.get("/", getGoal);
+
+router.post("/", postGoal);
+
+router.put("/:id", updateGoal);
+
+router.delete("/:id", deleteGoal);
+
+module.exports = router;
